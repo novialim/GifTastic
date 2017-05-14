77efbfa225e8ce70f72c6228c0c002bad6e4a99c
novialim <novialim@gmail.com>
2017-05-14 00:15:22 -0700

    updated to https

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function displayPixarGiphy() {
 	$("#pixarChar").empty();
 
 	var pixarChar = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         pixarChar + "&rating=g&api_key=dc6zaTOxFJmzC&limit=10";
 
 	$.ajax({
